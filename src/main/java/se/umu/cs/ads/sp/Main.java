02e19dbb1f673bdf7d01822cce43751dd345fc85
jalle1337 <97190981+jalle1337@users.noreply.github.com>
2024-09-21 11:06:37 +0200

    Added AppSettings

--- src/main/java/se/umu/cs/ads/sp/Main.java
+++ src/main/java/se/umu/cs/ads/sp/Main.java
@@ -1,7 +1,10 @@
-package se.umu.cs.ads.sp;
+package main.java.se.umu.cs.ads.sp;
+
+import main.java.se.umu.cs.ads.sp.utils.AppSettings;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AppSettings.SetGameConfig();
+        AppSettings.PrintSettings();
     }
 }
